5ae08ae230c76d81ee04a965a44af908dc531310
ArvinXY <arvinjunior@163.com>
2022-01-08 07:24:13 +0900

    feat(POJO): 添加了实体类
    添加了用户信息表的实体类

---
+++ src/main/java/com/arvin/mapleforestbackend/POJO/User.java
@@ -0,0 +1,39 @@
+package com.arvin.mapleforestbackend.POJO;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * 用户信息表的实体类
+ *
+ * @author arvin
+ */
+@Data
+public class User {
+    private Long userId;
+
+    private String userName;
+
+    private String nickName;
+
+    private String password;
+
+    private Integer phoneNumber;
+
+    private String email;
+
+    private Boolean status;
+
+    private Boolean delFlag;
+
+    private String createBy;
+
+    private Date createTime;
+
+    private String updateBy;
+
+    private Date updateTime;
+
+    private String remark;
+}
