3a9428822f833029fc6ba591f42f8aa0dafa04fc
ArvinXY <arvinjunior@163.com>
2022-01-08 08:42:48 +0900

    test(System): 测试了基础的系统运行
    测试了基础的系统运行，确认与数据库的连接没有问题

--- src/test/java/com/arvin/mapleforestbackend/MapleForestBackendApplicationTests.java
+++ src/test/java/com/arvin/mapleforestbackend/MapleForestBackendApplicationTests.java
@@ -1,13 +1,23 @@
 package com.arvin.mapleforestbackend;
 
+import com.arvin.mapleforestbackend.POJO.User;
+import com.arvin.mapleforestbackend.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class MapleForestBackendApplicationTests {
 
+	@Autowired
+	private UserMapper userMapper;
+
 	@Test
-	void contextLoads() {
+	void test01(){
+		final List<User> users = userMapper.selectAll();
+		System.out.println(users);
 	}
 
 }
